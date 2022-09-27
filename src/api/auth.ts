b22cfd65d1b85e36ce8b3df44f97cab033175cfe
syoo970 <syu9710@gmail.com>
2022-09-27 13:24:14 +0900

    [fix] 로그인 403 에러 수정

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 import { OAuthResponse } from "./interfaces/auth.interface";
 
 export const oauthLogin = async (authorizationCode: string): OAuthResponse => {
-  const result = await axios.post("/users/oauth/login", {
-    authorizationCode,
-  });
+  const result = await axios.post(
+    "/users/oauth/login",
+    { authorizationCode },
+    { headers: { "Access-Control-Allow-Origin": "*" } }
+  );
   return result.data;
 };
